8a4d1da4a56d09ae0f65b6c2eddbddf1dbed132d
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2025-02-17 18:36:34 +0300

    edited

--- src/pages/HeroPage/Hero.jsx
+++ src/pages/HeroPage/Hero.jsx
@@ -10,59 +10,49 @@ const Hero = ({ theme }) => {
   }
 
   return (
-    <div className="relative dark:bg-black dark:text-white duration-300 p-4">
+    <div className="relative dark:bg-black dark:text-white duration-300 p-4 min-h-screen flex items-center">
       <img
         src={bgImage}
         alt="HYD POINT"
         className="absolute inset-0 w-full h-full object-cover"
       />
       <div className="absolute inset-0 w-full h-full bg-black opacity-50"></div>
-      <div className="container min-h-[620px] flex relative z-10">
-        <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
-          <div
-            data-aos="zoom-in"
-            data-aos-duration="1500"
-            data-aos-once="false"
-            className="order-1 sm:order-2"
+      <div className="container flex flex-col sm:flex-row items-center justify-center relative z-10 text-center sm:text-left w-full px-4">
+        <div
+          data-aos="zoom-in"
+          data-aos-duration="1500"
+          data-aos-once="false"
+          className="order-1 sm:order-2 flex justify-center"
+        >
+          <img
+            src={theme === "dark" ? malzeme1 : malzeme2}
+            alt="Company Image"
+            className="sm:scale-125 relative max-h-[500px] drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]"
+          />
+        </div>
+        <div className="space-y-5 order-2 sm:order-1 max-w-2xl">
+          <p data-aos="fade-up" className="text-primary text-2xl font-serif">
+            Kolay ve Hızlı
+          </p>
+          <h1
+            data-aos="fade-up"
+            data-aos-delay="600"
+            className="text-4xl sm:text-5xl lg:text-7xl text-white font-semibold font-serif drop-shadow-md"
           >
-            <img
-              src={theme === "dark" ? malzeme1 : malzeme2}
-              alt="Company Image"
-              className="sm:scale-125 relative -z-10 max-h-[600px] drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]"
-            />
-          </div>
-          <div className="space-y-5 order-2 sm:order-1 sm:pr-32">
-            <p
-              data-aos="fade-up"
-              className="text-primary text-2xl font-serif"
-            >
-              Kolay ve Hızlı
-            </p>
-            <h1
-              data-aos="fade-up"
-              data-aos-delay="600"
-              className="text-5xl text-white lg:text-7xl font-semibold font-serif drop-shadow-md"
-            >
-              HYD POINT ENDUSTRIYEL
-            </h1>
-            <p
-              className="text-white text-lg"
-              data-aos="fade-up"
-              data-aos-delay="1000"
-            >
-              HYD Point, alım satım işlemlerinizi hızlı ve güvenli bir şekilde
-              yapmanızı sağlar. Yüksek kaliteli hizmet ve geniş ürün yelpazesi
-              ile sizlere en iyi pazarlama çözümleri sunuyoruz.
-            </p>
-            <button
-              data-aos="fade-up"
-              data-aos-delay="1500"
-              onClick={handleClick}
-              className="rounded-md bg-primary hover:bg-primary/80 transition duration-500 py-2 px-6 text-black"
-            >
-              Hemen Başla
-            </button>
-          </div>
+            HYD POINT ENDUSTRIYEL
+          </h1>
+          <p className="text-white text-lg" data-aos="fade-up" data-aos-delay="1000">
+            HYD Point, alım satım işlemlerinizi hızlı ve güvenli bir şekilde yapmanızı sağlar.
+            Yüksek kaliteli hizmet ve geniş ürün yelpazesi ile sizlere en iyi pazarlama çözümleri sunuyoruz.
+          </p>
+          <button
+            data-aos="fade-up"
+            data-aos-delay="1500"
+            onClick={handleClick}
+            className="rounded-md bg-primary hover:bg-primary/80 transition duration-500 py-2 px-6 text-black"
+          >
+            Hemen Başla
+          </button>
         </div>
       </div>
     </div>
--- src/pages/PageFlip.jsx
+++ src/pages/PageFlip.jsx
@@ -16,29 +16,29 @@ const Book = () => {
   const pages = chunkArray(machineList, 4);
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-gray-200 to-gray-100 dark:from-gray-800 dark:to-gray-900 p-4 sm:p-8">
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-gray-200 to-gray-100 dark:from-black dark:to-black p-4 sm:p-8">
       <HTMLFlipBook
         width={600}
         height={650}
         className="shadow-2xl rounded-lg overflow-hidden bg-gradient-to-r from-gray-700 dark:from-gray-900 dark:to-gray-800 to-white"
       >
-        <div className="relative w-full min-h-screen flex flex-col items-center justify-center text-white text-2xl font-bold px-10 space-y-36 mt-10">
+        <div className="relative w-full min-h-screen flex flex-col items-center justify-center text-white text-2xl font-bold px-36 space-y-36 mt-10">
           <img
             src={hyd}
             alt="Ürün Kataloğu"
-            className="w-full max-w-lg md:max-w-2xl lg:max-w-4xl h-auto object-contain p-16"
+            className="w-full max-w-lg md:max-w-2xl lg:max-w-4xl h-auto object-contain p-4 bg-transparent border dark:p-4 rounded-3xl"
           />
-          <div className="relative z-10 text-center">
-            <p className="text-3xl sm:text-4xl md:text-5xl font-serif">ÜRÜN KATALOĞU</p>
+          <div className="relative z-10 text-center space-y-4">
+            <p className="text-3xl sm:text-4xl md:text-5xl font-serif">ÜRÜN</p>
+            <p className="text-3xl sm:text-4xl md:text-5xl font-serif">KATALOĞU</p>
           </div>
         </div>
-
         {pages.map((products, index) => (
           <div
             key={index}
             className="relative w-full h-auto bg-gray-400 dark:bg-gray-900 p-6 shadow-lg"
           >
-            <h2 className="text-xl font-semibold text-center text-black mb-4">
+            <h2 className="text-xl font-semibold text-center text-black dark:text-white mb-4">
               Sayfa {index + 1}
             </h2>
             <div className="flex flex-wrap justify-center gap-4 px-2 xl:grid xl:grid-cols-2 xl:grid-rows-2 xl:gap-6">
@@ -51,24 +51,22 @@ const Book = () => {
                 </div>
               ))}
             </div>
-
-            <div className="absolute top-2 left-2 text-black hover:text-gray-700 cursor-pointer">
+            <div className="absolute top-2 left-2 text-black dark:text-white hover:text-gray-700 cursor-pointer">
               <ChevronLeft size={32} />
             </div>
-            <div className="absolute top-2 right-2 text-black hover:text-gray-700 cursor-pointer">
+            <div className="absolute top-2 right-2 text-black dark:text-white hover:text-gray-700 cursor-pointer">
               <ChevronRight size={32} />
             </div>
           </div>
         ))}
-
-        <div className="relative w-full h-full text-white text-xl font-bold p-10 flex flex-col items-center justify-center">
+        <div className="relative w-full min-h-screen flex flex-col items-center justify-center text-white text-2xl font-bold px-10 space-y-36  mt-10">
           <img
-            src="/assets/follow-us.jpg"
+            src={hyd}
             alt="Takip Edin"
-            className="w-full h-auto max-h-80 object-contain"
+            className="w-full max-w-lg md:max-w-2xl lg:max-w-4xl h-auto object-contain p-4 bg-transparent border dark:p-4 rounded-3xl"
           />
           <div className="relative z-10 text-center">
-            <p className="text-4xl font-serif">Daha fazla ürün için bizi takip edin! 🚀</p>
+            <p className="text-3xl sm:text-4xl md:text-5xl font-serif">Daha fazla ürün için bizi takip edin! 🚀</p>
           </div>
         </div>
       </HTMLFlipBook>
